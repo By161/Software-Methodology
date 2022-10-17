13898ede4bcc9af38804482fbaae2db56e3482f7
By161 <brandon00by@gmail.com>
2022-10-16 23:15:12 -0400

    Update Premium.java

--- Premium.java
+++ Premium.java
@@ -1,21 +1,20 @@
-import java.util.Calendar;
 import myPackage.Date;
 
-/**
- * required class to make the Premium membership object
-* @author Anna Kryzanekas, Brandon Yuen
- */
+import java.util.Calendar;
+
 public class Premium extends Family {
+
     private int numPremGuestPasses = 3; //decrease number every time you check in a guest
     private int ADD_ONE_YEAR = 1;
 
     /**
-     * Premium member constructor
-     * @param firstName
-     * @param lastName
-     * @param dob
+     * Creates a new instance of Member.
+     *
+     * @param firstName the first name of a member.
+     * @param lastName  the last name of a member.
+     * @param dob       the date of birth of a member.
      * @param expire
-     * @param location
+     * @param location  the member's gym location including town, county, and zip code.
      */
     public Premium(String firstName, String lastName, String dob, Date expire, String location) {
         super(firstName, lastName, dob, expire, location); //invoke the superclass constructor
@@ -35,7 +34,7 @@ public class Premium extends Family {
     }
 
     /**
-     * Method for membership fee of a premium member
+     *
      * @return
      */
     @Override
@@ -44,7 +43,7 @@ public class Premium extends Family {
     }
 
     /**
-     * toString method to show the data of the premium member
+     *
      * @return
      */
     @Override
