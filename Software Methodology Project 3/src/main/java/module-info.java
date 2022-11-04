804e37bb974c9db5bdc56436c028799c7b835a3b
By161 <92064135+By161@users.noreply.github.com>
2022-11-04 19:41:47 -0400

    Delete Software Methodology Project 3 directory

--- Software Methodology Project 3/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.project3_2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-
-
-    opens com.example.project3 to javafx.fxml;
-    exports com.example.project3;
-}
