b8d017a57dc21f77f9f7fa92b72624a9ba810376
By161 <92064135+By161@users.noreply.github.com>
2022-11-15 23:28:32 -0500

    Delete Project_4 directory

--- Project_4/src/main/java/com/example/project_4/PizzaFactory.java
+++
@@ -1,8 +0,0 @@
-package com.example.project_4;
-
-public interface PizzaFactory {
-    Pizza createDeluxe();
-    Pizza createMeatzza();
-    Pizza createBBQChicken();
-    Pizza createBuildYourOwn();
-}
