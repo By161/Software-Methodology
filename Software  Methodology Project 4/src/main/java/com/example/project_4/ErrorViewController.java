c0d5d8e25a8f15f06f7db1ccb6fb391d9a322ab1
By161 <92064135+By161@users.noreply.github.com>
2022-11-21 17:35:23 -0500

    Delete Software  Methodology Project 4 directory

--- Software  Methodology Project 4/src/main/java/com/example/project_4/ErrorViewController.java
+++
@@ -1,8 +0,0 @@
-package com.example.project_4;
-
-/**
- * Controller for the error window that pops up when an input is invalid
- * @author Brandon Yuen, Anna Kryzanekas
- */
-public class ErrorViewController {
-}
